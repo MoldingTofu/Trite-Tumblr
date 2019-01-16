c6d7605ebac6f0b0bdb55f3c9f5400cbffbfef31
moldingtofu <jeremykchang@gmail.com>
2019-01-16 18:24:07 -0500

    first commit

---
+++ Trite Tumblr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trite Tumblr
+//
+//  Created by jeremy on 1/16/19.
+//  Copyright © 2019 Jeremy Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Trite TumblrTests/Trite_TumblrTests.swift
@@ -0,0 +1,34 @@
+//
+//  Trite_TumblrTests.swift
+//  Trite TumblrTests
+//
+//  Created by jeremy on 1/16/19.
+//  Copyright © 2019 Jeremy Chang. All rights reserved.
+//
+
+import XCTest
+@testable import Trite_Tumblr
+
+class Trite_TumblrTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
